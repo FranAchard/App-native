f657f434a088064f7eeb1ac4f40be6550f64bfac
FranAchard <franachard8@gmail.com>
2023-11-07 17:00:55 -0300

    Home inicio

---
+++ Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="container-home">
+            <h1 className="title-home">Cine-Guía</h1>
+        </div>
+    );
+}
+
+export default Home;
